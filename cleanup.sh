f1b81f8259716b1d47e9f7772192641c15cfad93
Gleb Skalinskii <gleb.s@nakedideas.com>
2015-03-16 17:12:20 +0000

    Add deleting message

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-echo $DIR
+echo "Deleting git related files and cleanup script itself from $DIR"
 
 rm -rf $DIR/.git* $DIR/README.md $DIR/cleanup.sh 
